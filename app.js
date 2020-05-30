21e203529cd8cc2c7979be1e754158551a687db4
Ashish <as7220004@gmail.com>
2020-05-31 01:01:53 +0530

    APIs integrated and chained together

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
-//jshint esversion:6
+ //jshint esversion:6
 
-const request = require('postman-request');
-const url = 'http://api.weatherstack.com/current?access_key=6de605d7a713ef0779de2da9948500aa&query=28.613939,77.209023';
+ const geocode = require('./utils/geocode');
+ const forecast = require('./utils/forecast');
 
-request({url: url}, (error, response)=>{
-   
-    const data = JSON.parse(response.body);
-    console.log(data.current);
-});
+const address = process.argv[2];
+
+if(!address){
+    console.log('Please, provide an address...');
+} else{
+    geocode(address, (error, data)=>{
+        if(error){
+            return console.log(error);
+        }
+
+        forecast(data.latitude, data.longitude, (error, forecastData)=>{                       //Callback chaining
+        if(error){
+            return console.log(error);
+        }
+            console.log(data.location);
+            console.log(forecastData);
+        });
+    });
+}
