f662596e21a24cfc3a90bb04836a7fa1c31d6ece
Ashish <as7220004@gmail.com>
2020-06-02 18:12:40 +0530

    Templating done

--- web-server/src/index.js
+++ web-server/src/index.js
@@ -1,18 +1,40 @@
 //jshint esversion:6
-
 const express = require('express');
+const path = require('path');             //Core node.js module
+const hbs = require('hbs');
 
 const app = express();
 
-app.get('', (req, res)=>{
-    res.send('<h1>Weather App!</h1>>');
+//Defining paths for express.js configuration
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+
+//Setting up handlebars engine and views location
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+
+//Setup for static directory to serve
+app.use(express.static(publicDirectoryPath));
+
+app.get('', (req, res)=>{               //Setting route for index.hbs
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Ashish Singh'
+    });
 });
 
-app.get('/about', (req, res)=>{
-    res.send('<h1>About Us</h1>');
+app.get('/about', (req, res)=>{         //Setting route for about.hbs
+    res.render('about', {
+        title: 'About Us',
+        description: 'Developer',
+        name: 'Ashish Singh'
+    });
 });
 
-app.get('/weather', (req, res)=>{
+app.get('/weather', (req, res)=>{       //This will render JSON 
     res.send({
         forecast: 'Partly cloudy',
         location: 'New Delhi'
