38ab95dbb3820c56854fecfda81cef75b51f5b52
Ashish <as7220004@gmail.com>
2020-06-03 02:09:39 +0530

    Frontend and Backend connected together

---
+++ web-server/public/js/clientSide.js
@@ -0,0 +1,27 @@
+//jshint esversion:6
+console.log('Client side javascript!');
+
+const weatherForm = document.querySelector('form');
+const searchElement = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const location = searchElement.value;
+
+    messageOne.textContent = 'Loading...';
+    //Fetch API
+fetch('http://localhost:3000/weather?address=' + location).then((response)=>{
+    response.json().then((data)=>{
+        if(data.error){
+            messageTwo.textContent = data.error;
+        } else{
+            messageOne.textContent = data.location;
+            messageTwo.textContent = data.forecast;
+        }
+        
+    });
+}); 
+});
--- web-server/src/index.js
+++ web-server/src/index.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const path = require('path');             //Core node.js module
 const hbs = require('hbs');
+const geocode = require('../../utils/geocode');
+const forecast = require('../../utils/forecast');
 
 const app = express();
 
@@ -35,9 +37,44 @@ app.get('/about', (req, res)=>{         //Setting route for about.hbs
 });
 
 app.get('/weather', (req, res)=>{       //This will render JSON 
+    if(!req.query.address){
+       return res.send({ 
+            error: 'You must provide an address'
+        });
+    }
+    
+    geocode(req.query.address, (error, {latitude, longitude, location} = {})=>{
+        if(error){
+            return res.send({
+                error: error
+            });
+        }
+        forecast(latitude, longitude, (error, forecastData)=>{
+            if(error){
+                return res.send({
+                    error: error
+                });
+            }
+            res.send({
+                forecast: forecastData,
+                location: location,
+                address: req.query.address
+
+            });
+        });
+    });
+});
+
+app.get('/products', (req, res)=>{
+    if(!req.query.search){
+      return res.send({
+            error: 'You must provide search term.'
+        });
+    }
+    console.log(req.query);
+   
     res.send({
-        forecast: 'Partly cloudy',
-        location: 'New Delhi'
+        products: []
     });
 });
 
